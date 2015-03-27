495eb20718079d8021a71d88bb83fcfa2f8541c8
L. Oosthuizen <leneoosthuizen1@gmail.com>
2015-03-27 11:16:00 +0200

    MM Oosthuizen bin file commit

--- bin/count_name.py
+++ bin/count_name.py
@@ -1,9 +1,11 @@
-#!/usr/bin/eenv python
+#!/usr/bin/env python
 
 import sys
 
+#sys.path.append(~/Document/python/python2015_test/bin/)
+
 def count_name(filename, protein_name):
-    input_file = open(sys.argv[1])
+    input_file = open(filename)
     count = 0
     for line in input_file:
         if line.rstrip() == protein_name:
@@ -13,7 +15,10 @@ def count_name(filename, protein_name):
 if len(sys.argv) != 3:
     sys.exit("Usage: count_name.py <protein names file> <protein name>")
 
-filename = sys.argv[0]
-protein_name = sys.argv[1]
+filename = sys.argv[1]
+protein_name = sys.argv[2]
 name_count = count_name(filename, protein_name)
+
 print protein_name, name_count
+
+
